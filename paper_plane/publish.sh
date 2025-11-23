ec798afc5158fa03e25d70c0ab16da52ba4c473f
gjx <dyingforge@gjxdeMacBook-Air.local>
2025-11-23 15:38:09 +0800

    update contract

---
+++ paper_plane/publish.sh
@@ -0,0 +1,174 @@
+Transaction Digest: 5XzbzkQg2Y2hVmwf74nx4is5kgA7esiv6H23ev8aZx2R
+╭──────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Transaction Data                                                                                             │
+├──────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Sender: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                                   │
+│ Gas Owner: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                                │
+│ Gas Budget: 38510400 MIST                                                                                    │
+│ Gas Price: 1000 MIST                                                                                         │
+│ Gas Payment:                                                                                                 │
+│  ┌──                                                                                                         │
+│  │ ID: 0x01e504389b44643e22a5b849c439094031f2ab3aca343b8450bf1d0343e55f1e                                    │
+│  │ Version: 654856612                                                                                        │
+│  │ Digest: 8W9CxDtkHqgJCXv3SejWA8hP6n7RQgiSbGjNqRA9sQSX                                                      │
+│  └──                                                                                                         │
+│                                                                                                              │
+│ Transaction Kind: Programmable                                                                               │
+│ ╭──────────────────────────────────────────────────────────────────────────────────────────────────────────╮ │
+│ │ Input Objects                                                                                            │ │
+│ ├──────────────────────────────────────────────────────────────────────────────────────────────────────────┤ │
+│ │ 0   Pure Arg: Type: address, Value: "0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7" │ │
+│ ╰──────────────────────────────────────────────────────────────────────────────────────────────────────────╯ │
+│ ╭─────────────────────────────────────────────────────────────────────────╮                                  │
+│ │ Commands                                                                │                                  │
+│ ├─────────────────────────────────────────────────────────────────────────┤                                  │
+│ │ 0  Publish:                                                             │                                  │
+│ │  ┌                                                                      │                                  │
+│ │  │ Dependencies:                                                        │                                  │
+│ │  │   0x0000000000000000000000000000000000000000000000000000000000000001 │                                  │
+│ │  │   0x0000000000000000000000000000000000000000000000000000000000000002 │                                  │
+│ │  │   0x4408a42943961f17f805a05fff08c805eb8ee68bb43796773a4514a3ec849aca │                                  │
+│ │  └                                                                      │                                  │
+│ │                                                                         │                                  │
+│ │ 1  TransferObjects:                                                     │                                  │
+│ │  ┌                                                                      │                                  │
+│ │  │ Arguments:                                                           │                                  │
+│ │  │   Result 0                                                           │                                  │
+│ │  │ Address: Input  0                                                    │                                  │
+│ │  └                                                                      │                                  │
+│ ╰─────────────────────────────────────────────────────────────────────────╯                                  │
+│                                                                                                              │
+│ Signatures:                                                                                                  │
+│    0aLt/fySXdDq0z+cYtUwLqstKficdhJBsQ7Nnbwz+b9VuQcUnOng1KHDKyRUqVGtMdC/wJLXgTan4dhXrU44DA==                  │
+│                                                                                                              │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭───────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Transaction Effects                                                                               │
+├───────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Digest: 5XzbzkQg2Y2hVmwf74nx4is5kgA7esiv6H23ev8aZx2R                                              │
+│ Status: Success                                                                                   │
+│ Executed Epoch: 926                                                                               │
+│                                                                                                   │
+│ Created Objects:                                                                                  │
+│  ┌──                                                                                              │
+│  │ ID: 0x280c7273508f8772e0b359d3d1897c2205dfeec467c6b4a304ff69f32e6dfbcd                         │
+│  │ Owner: Immutable                                                                               │
+│  │ Version: 1                                                                                     │
+│  │ Digest: D2Ns1bbEwECMpyV85qEXvHgXVQTRYibN58yUQDsrYekc                                           │
+│  └──                                                                                              │
+│  ┌──                                                                                              │
+│  │ ID: 0x49284dfe3c92fc91b305d2fd9589878c55987251c97e37a9dedc0e1c785e88f2                         │
+│  │ Owner: Account Address ( 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7 )  │
+│  │ Version: 654856613                                                                             │
+│  │ Digest: BkFcrGPbtVseoYhwj8yc7Fi1ckKc2KfN1cD36rd54xUs                                           │
+│  └──                                                                                              │
+│  ┌──                                                                                              │
+│  │ ID: 0x618b2d93bdd35ecd10a1d396fd8082503c7721e5d93f28c6e0f06cea504772ba                         │
+│  │ Owner: Shared( 654856613 )                                                                     │
+│  │ Version: 654856613                                                                             │
+│  │ Digest: 5GxE7mqGaUSC5hGBBhihK8HRczjLTgri4GCmUPA8NBy9                                           │
+│  └──                                                                                              │
+│  ┌──                                                                                              │
+│  │ ID: 0xad50f0052805c26e591e5241425433cb9e04860e62955d6acbda638afae62e14                         │
+│  │ Owner: Shared( 654856613 )                                                                     │
+│  │ Version: 654856613                                                                             │
+│  │ Digest: Fqo9dGhSuKsNwUXPC6bzsAnsjbVDMLtXPP2W12mpvHfs                                           │
+│  └──                                                                                              │
+│  ┌──                                                                                              │
+│  │ ID: 0xd2dc475bb9446dbab7cd5d9008c7a47db81fcd653428cec5a9a82b4982f3c323                         │
+│  │ Owner: Shared( 654856613 )                                                                     │
+│  │ Version: 654856613                                                                             │
+│  │ Digest: C2YdPKkZUmALbwU3pCAVpfTcFzHwrYxnoKaVfStbXXZZ                                           │
+│  └──                                                                                              │
+│ Mutated Objects:                                                                                  │
+│  ┌──                                                                                              │
+│  │ ID: 0x01e504389b44643e22a5b849c439094031f2ab3aca343b8450bf1d0343e55f1e                         │
+│  │ Owner: Account Address ( 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7 )  │
+│  │ Version: 654856613                                                                             │
+│  │ Digest: E2HFvH7NGNrUSLTDFYrJVkfcEbsuYGC3JVZjXwerZSGS                                           │
+│  └──                                                                                              │
+│ Gas Object:                                                                                       │
+│  ┌──                                                                                              │
+│  │ ID: 0x01e504389b44643e22a5b849c439094031f2ab3aca343b8450bf1d0343e55f1e                         │
+│  │ Owner: Account Address ( 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7 )  │
+│  │ Version: 654856613                                                                             │
+│  │ Digest: E2HFvH7NGNrUSLTDFYrJVkfcEbsuYGC3JVZjXwerZSGS                                           │
+│  └──                                                                                              │
+│ Gas Cost Summary:                                                                                 │
+│    Storage Cost: 36510400 MIST                                                                    │
+│    Computation Cost: 1000000 MIST                                                                 │
+│    Storage Rebate: 978120 MIST                                                                    │
+│    Non-refundable Storage Fee: 9880 MIST                                                          │
+│                                                                                                   │
+│ Transaction Dependencies:                                                                         │
+│    9D3Ypemh6DadH4weuCeYPjXD2nGsPpb5L8cNteZcUFyA                                                   │
+│    ACz78mhTfHabiBfg6gJ94w2KbAZB9rWLNPvh3KmMoS1g                                                   │
+│    DCvwEquvBeAtCgccBanBAeC7pTLGL7SXkXnqzXDkh3Zg                                                   │
+│    Dd9pn1zFcSJjinxQewFd2gQdR4XKsHxFioD5MYnwLZQz                                                   │
+╰───────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭─────────────────────────────╮
+│ No transaction block events │
+╰─────────────────────────────╯
+
+╭─────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Object Changes                                                                                              │
+├─────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Created Objects:                                                                                            │
+│  ┌──                                                                                                        │
+│  │ ObjectID: 0x49284dfe3c92fc91b305d2fd9589878c55987251c97e37a9dedc0e1c785e88f2                             │
+│  │ Sender: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                               │
+│  │ Owner: Account Address ( 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7 )            │
+│  │ ObjectType: 0x2::package::UpgradeCap                                                                     │
+│  │ Version: 654856613                                                                                       │
+│  │ Digest: BkFcrGPbtVseoYhwj8yc7Fi1ckKc2KfN1cD36rd54xUs                                                     │
+│  └──                                                                                                        │
+│  ┌──                                                                                                        │
+│  │ ObjectID: 0x618b2d93bdd35ecd10a1d396fd8082503c7721e5d93f28c6e0f06cea504772ba                             │
+│  │ Sender: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                               │
+│  │ Owner: Shared( 654856613 )                                                                               │
+│  │ ObjectType: 0x280c7273508f8772e0b359d3d1897c2205dfeec467c6b4a304ff69f32e6dfbcd::paper_plane::SealConfig  │
+│  │ Version: 654856613                                                                                       │
+│  │ Digest: 5GxE7mqGaUSC5hGBBhihK8HRczjLTgri4GCmUPA8NBy9                                                     │
+│  └──                                                                                                        │
+│  ┌──                                                                                                        │
+│  │ ObjectID: 0xad50f0052805c26e591e5241425433cb9e04860e62955d6acbda638afae62e14                             │
+│  │ Sender: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                               │
+│  │ Owner: Shared( 654856613 )                                                                               │
+│  │ ObjectType: 0x280c7273508f8772e0b359d3d1897c2205dfeec467c6b4a304ff69f32e6dfbcd::paper_plane::SetAdmin    │
+│  │ Version: 654856613                                                                                       │
+│  │ Digest: Fqo9dGhSuKsNwUXPC6bzsAnsjbVDMLtXPP2W12mpvHfs                                                     │
+│  └──                                                                                                        │
+│  ┌──                                                                                                        │
+│  │ ObjectID: 0xd2dc475bb9446dbab7cd5d9008c7a47db81fcd653428cec5a9a82b4982f3c323                             │
+│  │ Sender: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                               │
+│  │ Owner: Shared( 654856613 )                                                                               │
+│  │ ObjectType: 0x280c7273508f8772e0b359d3d1897c2205dfeec467c6b4a304ff69f32e6dfbcd::paper_plane::Airport     │
+│  │ Version: 654856613                                                                                       │
+│  │ Digest: C2YdPKkZUmALbwU3pCAVpfTcFzHwrYxnoKaVfStbXXZZ                                                     │
+│  └──                                                                                                        │
+│ Mutated Objects:                                                                                            │
+│  ┌──                                                                                                        │
+│  │ ObjectID: 0x01e504389b44643e22a5b849c439094031f2ab3aca343b8450bf1d0343e55f1e                             │
+│  │ Sender: 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7                               │
+│  │ Owner: Account Address ( 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7 )            │
+│  │ ObjectType: 0x2::coin::Coin<0x2::sui::SUI>                                                               │
+│  │ Version: 654856613                                                                                       │
+│  │ Digest: E2HFvH7NGNrUSLTDFYrJVkfcEbsuYGC3JVZjXwerZSGS                                                     │
+│  └──                                                                                                        │
+│ Published Objects:                                                                                          │
+│  ┌──                                                                                                        │
+│  │ PackageID: 0x280c7273508f8772e0b359d3d1897c2205dfeec467c6b4a304ff69f32e6dfbcd                            │
+│  │ Version: 1                                                                                               │
+│  │ Digest: D2Ns1bbEwECMpyV85qEXvHgXVQTRYibN58yUQDsrYekc                                                     │
+│  │ Modules: paper_plane, utils                                                                              │
+│  └──                                                                                                        │
+╰─────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭───────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Balance Changes                                                                                   │
+├───────────────────────────────────────────────────────────────────────────────────────────────────┤
+│  ┌──                                                                                              │
+│  │ Owner: Account Address ( 0x5e1fc794234ba7ea835930215797c83536f5de51388f181cf1f4f08120dcabd7 )  │
+│  │ CoinType: 0x2::sui::SUI                                                                        │
+│  │ Amount: -36532280                                                                              │
+│  └──                                                                                              │
+╰───────────────────────────────────────────────────────────────────────────────────────────────────╯
--- paper_plane/sources/paper_plane.move
+++ paper_plane/sources/paper_plane.move
@@ -1,59 +1,46 @@
 module paper_plane::paper_plane;
 
 use paper_plane::utils::{to_b36, is_prefix};
-use seal::bf_hmac_encryption::{
-    EncryptedObject,
-    decrypt,
-    new_public_key,
-    verify_derived_keys,
-    parse_encrypted_object
-};
 use std::string::String;
-use sui::random::{Self, Random};
-use sui::balance::{Self, Balance};
-use sui::bls12381::g1_from_bytes;
+use sui::random::{ Random};
 use sui::clock::Clock;
-use sui::coin::{Self, Coin};
 use sui::event;
-use sui::sui::SUI;
 use sui::table::{Self, Table};
 
 
 const EAlreadyPicked: u64 = 0;
 const ENoAccess: u64 = 1;
 const EInvalidAirplane: u64 = 2;
-const EInvalidEncryptedComment: u64 = 3;
+const EInvalidFlyLastingTime: u64 = 3;
 
+const MIN_FLY_LASTING_TIME: u64 = 1000 * 60 * 10;
 const PICK_INTERVAL: u64 = 1000 * 60 * 5; 
 
-
-public struct SealConfig has key {
+public struct SetAdmin has key {
     id: UID,
-    package_id: address,
-    key_servers: vector<address>,
-    publickeys: vector<vector<u8>>,
-    threshold: u8,
 }
-
 public struct Airplane has key, store {
     id: UID,
     name: String,
     owner: address,
     content_blob: String,
+    start_time: u64,
+    fly_lasting_time: u64,
     picked_by: address,
     picked_time: u64,
     picked_count: u64,
-    comments: vector<EncryptedObject>,
+    comments: vector<String>,
     b36addr: String,
 }
 
 public struct Airport has key, store {
     id: UID,
     lasting_picked_time: u64,
-    //airplane address -> picked_by address
-    airplanes_picked: Table<address, address>,
+    //owner address -> airplane address
+    airplanes_owned: Table<address, vector<address>>,
+    airplanes_picked: Table<address, vector<address>>,
     //airplane address -> airplane object
-    airplanes: vector<Airplane>,
+    airplanes: vector<Airplane>,    
 }
 
 /*-----事件------*/
@@ -64,31 +51,31 @@ public struct CreatedAirplaneEvent has copy, drop {
 }
 
 fun init(ctx: &mut TxContext) {
-    let seal_config = SealConfig {
-        id: object::new(ctx),
-        package_id: @paper_plane,
-        key_servers: vector::empty(),
-        publickeys: vector::empty(),
-        threshold: 0,
-    };
     let airport = Airport {
         id: object::new(ctx),
         lasting_picked_time: 0,
+        airplanes_owned: table::new(ctx),
         airplanes_picked: table::new(ctx),
         airplanes: vector::empty(),
     };
 
-    transfer::share_object(seal_config);
+    let set_admin = SetAdmin {
+        id: object::new(ctx),
+    };
+
+    transfer::share_object(set_admin);
     transfer::share_object(airport);
 }
 
 public fun create_airplane(
     airport: &mut Airport,
     name: String,
+    fly_lasting_time: u64,
     content: String,
     clock: &Clock,
     ctx: &mut TxContext,
 ) {
+    assert!(fly_lasting_time >= MIN_FLY_LASTING_TIME, EInvalidFlyLastingTime);
     let sender = ctx.sender();
     let id = object::new(ctx);
     let object_address = object::uid_to_address(&id);
@@ -100,6 +87,8 @@ public fun create_airplane(
         name,
         owner: sender,
         content_blob: content,
+        fly_lasting_time,
+        start_time: clock.timestamp_ms(),
         picked_by: sender, 
         picked_time: clock.timestamp_ms(),
         picked_count: 0,
@@ -107,8 +96,15 @@ public fun create_airplane(
         b36addr: b36addr,
     };
 
-    table::add(&mut airport.airplanes_picked, object_address, sender);
     vector::push_back(&mut airport.airplanes, airplane);
+    if (table::contains(&airport.airplanes_owned, sender)) {    
+        let airplanes_owned_ref = table::borrow_mut(&mut airport.airplanes_owned, sender);
+        vector::push_back(&mut *airplanes_owned_ref, object_address);
+    } else {
+        let mut airplanes_owned_ref = vector::empty();
+        vector::push_back(&mut airplanes_owned_ref, object_address);
+        table::add(&mut airport.airplanes_owned, sender, airplanes_owned_ref);
+    };
 
     event::emit(CreatedAirplaneEvent {
         event_id,
@@ -119,22 +115,11 @@ public fun create_airplane(
 
 public fun add_comment(
     airplane: &mut Airplane,
-    crypto_comment_data: vector<u8>,
-    seal_config: &SealConfig,
+    comment: String,
     ctx: &mut TxContext,
 ) {
     assert!(airplane.picked_by == ctx.sender(), ENoAccess);
-    
-    let crypto_comment = parse_encrypted_object(crypto_comment_data);
-    let airplane_id = object::id(airplane).to_bytes();
-    
-    assert!(*crypto_comment.id() == airplane_id, EInvalidEncryptedComment);
-    assert!(*crypto_comment.services() == seal_config.key_servers, EInvalidEncryptedComment);
-    assert!(crypto_comment.threshold() == seal_config.threshold, EInvalidEncryptedComment);
-    assert!(*crypto_comment.package_id() == seal_config.package_id, EInvalidEncryptedComment);
-    assert!(crypto_comment.aad().borrow() == ctx.sender().to_bytes(), EInvalidEncryptedComment);
-    
-    vector::push_back(&mut airplane.comments, crypto_comment);
+    vector::push_back(&mut airplane.comments, comment);
 }
 
 public fun pick_plane(
@@ -156,16 +141,30 @@ public fun pick_plane(
     
     assert!(current_time >= airplane.picked_time + PICK_INTERVAL, EAlreadyPicked);
     
+    let old_picked_by = airplane.picked_by;
+    if (old_picked_by == ctx.sender()) {
+        return
+    };
     airplane.picked_by = ctx.sender();
     airplane.picked_time = current_time;
     airplane.picked_count = airplane.picked_count + 1;
-    
-    if (table::contains(&airport.airplanes_picked, airplane_address)) {
-        let picked_by_ref = table::borrow_mut(&mut airport.airplanes_picked, airplane_address);
-        *picked_by_ref = ctx.sender();
+
+    if (table::contains(&airport.airplanes_picked, ctx.sender())) {
+
+        let picked_by_ref = table::borrow_mut(&mut airport.airplanes_picked, ctx.sender());
+        vector::push_back(&mut *picked_by_ref, airplane_address);
     } else {
-        table::add(&mut airport.airplanes_picked, airplane_address, ctx.sender());
+        let mut airplanes_picked_ref = vector::empty();
+        vector::push_back(&mut airplanes_picked_ref, airplane_address);
+        table::add(&mut airport.airplanes_picked, ctx.sender(), airplanes_picked_ref);
     };
+        if (table::contains(&airport.airplanes_picked, old_picked_by)) {
+            let old_list = table::borrow_mut(&mut airport.airplanes_picked, old_picked_by);
+            let (found, index) = vector::index_of(old_list, &airplane_address);
+            if (found) {
+                vector::remove(old_list, index);
+            };
+        };
 }
 
 public fun namespace(airplane: &Airplane): vector<u8> {
@@ -184,7 +183,7 @@ public fun approve_internal(
     };
     
     let current_time = clock.timestamp_ms();
-    let end_time = airplane.picked_time + PICK_INTERVAL;
+    let end_time = airplane.start_time + airplane.fly_lasting_time;
     
     if (airplane.picked_by == ctx.sender()) {
         return true
@@ -202,4 +201,42 @@ entry fun seal_approve(
     ctx: &mut TxContext,
 ) {
     assert!(approve_internal(airplane, id, clock, ctx), ENoAccess);
+}
+
+//owned airplanes
+public fun get_user_airplanes(
+    airport: &Airport,
+    user: address,
+) : vector<address> {
+    if (table::contains(&airport.airplanes_owned, user)) {
+        let airplanes_ref = table::borrow(&airport.airplanes_owned, user);
+        *airplanes_ref
+    } else {
+        vector::empty()
+    }
+}
+
+public fun get_picked_airplanes(
+    airport: &Airport,
+    user: address,
+) : vector<address> {
+    if (table::contains(&airport.airplanes_picked, user)) {
+        let airplanes_ref = table::borrow(&airport.airplanes_picked, user);
+        *airplanes_ref
+    } else {
+        vector::empty()
+    }
+}
+
+public fun get_airplane_return_time(
+    airplane: &Airplane,
+    clock: &Clock,
+) : u64 {
+    let current_time = clock.timestamp_ms();
+    let return_time = airplane.start_time + airplane.fly_lasting_time;
+    if (current_time >= return_time) {
+        return return_time
+    } else {
+        return 0
+    }
 }
